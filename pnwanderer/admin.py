c0d064f2112a47efd12475c434849c531b4332e6
ssmith362 <spencey7@gmail.com>
2023-06-29 19:25:07 -0700

    Got my details page properly working

--- pnwanderer/admin.py
+++ pnwanderer/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost, PostImage
 
 
 admin.site.register(BlogPost)
+
+admin.site.register(PostImage)
--- pnwanderer/models.py
+++ pnwanderer/models.py
@@ -5,9 +5,15 @@ class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='featured_image/%Y/%m/%d/', blank=True, null=True)
-    content = models.TextField()
+    summary = models.TextField(max_length=2000)
+    full_description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+    
+class PostImage(models.Model):
+    post = models.ForeignKey(BlogPost, default=None, on_delete=models.CASCADE, related_name='images')
+    images = models.ImageField(upload_to = 'blog_galleries/%Y/%m/%d/')
+    def __str__(self):
+        return self.post.title
